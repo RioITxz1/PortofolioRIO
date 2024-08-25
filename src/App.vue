3762208b601fc527b099a7257b340d181bb55f97
RioITxz1 <riozavela098@gmail.com>
2024-08-26 02:40:31 +0700

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
           <span class="self-center text-lg text-[#ffdb70] font-semibold whitespace-nowrap fadein-bot hover:text-amber-100">RioZavela();</span>
         </button>
         <div class="flex md:order-2 fadein-bot">
-          <a href="https://www.instagram.com/rrzavl136?igsh=MWZneWVvdWZjbjhrbg=="><img class="w-9 rounded-full" src="https://www.flaticon.com/free-icon/instagram_1384015" alt="github"></a>
+          <a href="https://www.instagram.com/rrzavl136?igsh=MWZneWVvdWZjbjhrbg=="><img class="w-9 rounded-full" src="/src/1.png" alt="github"></a>
         </div>
         <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
           <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
